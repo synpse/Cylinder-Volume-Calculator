b98b385cfac23a5b2ffcfed5b9b747f5deaed0e5
synpse <tfa@sapo.pt>
2018-02-22 17:33:40 +0000

    Add Cylinder Volume Calculator

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Test();
+        }
+
+        static void Test()
+        {
+            double a; // Area
+            double r; // Radius
+            double v; // Volume
+            
+            // Ask for area
+            Console.WriteLine("Cylinder area: ");
+            a = double.Parse(Console.ReadLine());
+
+            // Ask for volume
+            Console.WriteLine("\nCylinder radius: ");
+            r = double.Parse(Console.ReadLine());
+
+
+            // Calculate and print
+            v = Math.PI * r * a;
+            Console.WriteLine("\nThe cylinder´s volume is: " + v);
+
+            // Restart Program (loop)
+            Console.ReadKey();
+            Console.Clear();
+            Test();
         }
     }
 }
